e584ebeda96e279e8b3e126072fc1a871367d8e3
Nick Chubb <Nick.r.chubb@gmail.com>
2020-07-19 15:17:11 -0700

    remove garbage code

--- src/main.rs
+++ src/main.rs
@@ -133,34 +133,4 @@ fn main() {
 
     }
 
-
-
-    /* CODE FROM TUTORIAL, IGNORE ONLY FOR REFERENCE LOL
-    loop {
-        println!("Please input your guess");
-
-        let mut guess = String::new();
-
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
-
-        let guess: u32 = match guess.trim().parse() {
-	    Ok(num) => num,
-	    Err(_) => continue,
-	};
-
-        println!("You guessed: {}", guess);
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-		println!("You win!");
-		break;
-	    }
-        }
-    }
-    */
-
 }
