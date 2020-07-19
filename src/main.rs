6b7b212e2e4bc8e640caae7922c893f89fda9928
Nick Chubb <Nick.r.chubb@gmail.com>
2020-07-19 15:16:40 -0700

    Working convert_2d() funct and take user input

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-//use std::io;
+use std::io;
 //use rand::Rng;
+use regex::Regex;
 
 /* TO DISPLAY PRINT PROPERLY IMPLEMENT LATER
 impl<T> std::fmt::Display for Vec<T> {
@@ -26,7 +27,7 @@ impl Board {
             width: w,
             height: h,
             board_size: size,
-            board: vec![0; (size - 1).into()] //must convert u8 type -> usize type
+            board: vec![0; (size).into()] //must convert u8 type -> usize type
         }
     }
 
@@ -40,13 +41,14 @@ impl Board {
             print!("{} ", i);
             count += 1;
         }
+        println!();
     }
 
     fn ins(&mut self, pos: usize, val: u8) {
-        self.board.insert(pos, val)
+        self.board.splice(pos..pos+1, [val].iter().cloned());
     }
 
-    fn remove(&mut self, pos: usize){
+    fn rm(&mut self, pos: usize){
         self.board.insert(pos, 0)
     }
 
@@ -57,7 +59,42 @@ impl Board {
             self.board.insert(pos, 1)
         }
     }
+}
 
+fn convert_2d(s: &str) -> usize{
+    // pattern match a - h, 1- 8
+    // ()
+
+    let letter = s.chars().next().unwrap().to_ascii_lowercase();
+    let num = s.chars().nth(1).unwrap();
+
+    let col: usize = match letter {
+        'a' => 0,
+        'b' => 1,
+        'c' => 2,
+        'd' => 3,
+        'e' => 4,
+        'f' => 5,
+        'g' => 6,
+        'h' => 7,
+        _ => 42
+    };
+
+    // Probably better way to do this.... but I couldn't find it
+    let row: usize = match num {
+        '1' => 0,
+        '2' => 1,
+        '3' => 2,
+        '4' => 3,
+        '5' => 4,
+        '6' => 5,
+        '7' => 6,
+        '8' => 7,
+        _ => 42
+    };
+
+    row * 8 + col
+    
 }
 
 /**
@@ -73,11 +110,29 @@ fn main() {
 
     let width = 8;
     let height = 8;
-
     let mut board = Board::new(width, height);
+    let re = Regex::new(r"([aA-hH][1-8])").unwrap();
 
-    board.ins(5, 7);
-    board.print();
+    loop{
+
+        board.print();
+
+        println!("Place piece at position: ");
+
+        let mut input = String::new();
+
+        io::stdin().read_line(&mut input).expect("Failed to read line");
+
+        let res: usize = match re.is_match(&input) {
+            true => convert_2d(&input),
+            false => {println!("ERROR: invalid input"); continue},
+        };
+
+        board.ins(res, 1);
+
+
+    }
+
 
 
     /* CODE FROM TUTORIAL, IGNORE ONLY FOR REFERENCE LOL
